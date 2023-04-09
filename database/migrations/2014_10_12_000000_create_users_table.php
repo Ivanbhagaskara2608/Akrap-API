0420f649b338fe94bebe644894f115fb91d2c6f9
Ivanbhagaskara2608 <ivanbhagas2003@gmail.com>
2023-04-09 12:26:15 +0700

    init

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('userId');
+            $table->string('fullName', 60);
+            $table->string('phoneNumber', 13)->unique();
+            $table->date('birthdate');
+            $table->enum('gender', ['male', 'female']);
+            $table->string('username', 16)->unique();
+            $table->enum('type', ['ketua', 'wakil ketua', 'sekretaris', 'bendahara', 'anggota']);
+            $table->string('password');
+            $table->string('api_token')->unique()->nullable()->default(null);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
